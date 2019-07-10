3d103f62a61d5b31665b3b9b26eacd512c6e8451
hareshsinh <haresh.sisodiya@viitorcloud.com>
2019-07-10 18:03:42 +0530

    new change

--- app/Http/Controllers/API/CalculateController.php
+++ app/Http/Controllers/API/CalculateController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\API\BaseController as BaseController;
+use Validator;
 
 
 class CalculateController extends BaseController
@@ -18,7 +19,19 @@ class CalculateController extends BaseController
     public function index(Request $request)
     {
         try{
+            $input = $request->all();
 
+
+            $validator = Validator::make($input, [
+                'value1' => 'required',
+                'value2' => 'required',
+                'options' => 'required'
+            ]);
+
+
+            if($validator->fails()){
+                return $this->sendError('Validation Error.', $validator->errors());
+            }
 
         $response = '';
         if(isset($request->options) && $request->options!=''){
